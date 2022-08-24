cbf7099151036cbaeb393c075b9be0da81a96ff8
Levi Barsis <lbarsis@gmail.com>
2022-08-23 19:52:18 -0700

    Started Assignment, need to figure out how to like different sections. Can only like first heart

--- main.js
+++ main.js
@@ -3,8 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+function renderLike() {
+  const heart = document.querySelector('.like-glyph');
+  heart.addEventListener('click', (e) => {
+    e.preventDefault()
+    mimicServerCall().then(buildResponse).catch(catchError)
+  })
+}
 
+function buildResponse() {
+  const heart = document.querySelector('.like-glyph');
+  if (heart.textContent === EMPTY_HEART) {
+    heart.textContent = FULL_HEART
+    heart.classList.add('activated-heart')
+  }
+  else {
+    heart.textContent = EMPTY_HEART
+    heart.classList.remove('activated-heart')
+  }
+}
 
+function catchError(error) {
+  const modal = document.querySelector('#modal')
+  modal.classList.remove('hidden')
+  modal.textContent = error
+  setTimeout(() => {
+    modal.className = 'hidden'
+  }, 3000);
+}
+
+document.addEventListener('DOMContentLoaded', renderLike)
 
 
 //------------------------------------------------------------------------------
